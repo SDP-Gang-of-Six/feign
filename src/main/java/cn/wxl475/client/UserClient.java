7840c6b7cc7efe0ae7639c103b3f5fb8bd68d812
pc <2528578653@qq.com>
2024-04-16 18:04:06 +0800

    userClient接口添加

---
+++ src/main/java/cn/wxl475/client/UserClient.java
@@ -0,0 +1,14 @@
+package cn.wxl475.client;
+
+import cn.wxl475.pojo.Result;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.ArrayList;
+
+@FeignClient("user")
+public interface UserClient {
+    @GetMapping("/user/getNicknameById/{uid}")
+    Result getNicknameById(@RequestHeader("Authorization") String token, @PathVariable Long uid);
+}
